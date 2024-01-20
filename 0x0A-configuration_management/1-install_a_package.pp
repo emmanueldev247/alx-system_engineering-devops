84dfdb18601837ab4db59aa2722651afdc15d72e
Emmanuel Ademola <mailemmydee@gmail.com>
2024-01-20 06:54:09 -0800

    updated: ensured py & pip3 are installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-# Using Puppet to install flask from pip3
-package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+# Ensure Python3 and pip3 are installed
+package { 'python3':
+    ensure => installed,
 }
+
+package { 'python3-pip':
+    ensure  => installed,
+    require => Package['python3'],
+}
+
+# Install Flask version 2.1.0 using pip3
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    require  => Package['python3-pip'],
+}
+
