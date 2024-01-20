204d3904d8f88f3e32ee511edefa5d36a35f231b
Emmanuel Ademola <mailemmydee@gmail.com>
2024-01-20 06:39:17 -0800

    updated: taken 'required' out

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Using Puppet to install flask from pip3
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    require  => Package['python3-pip']
+package {'flask':
+    ensure  => '2.1.0',
+    provider => 'pip3'
 }
