83410ae6aaa6e605fe59d1024b823d77b4e29695
emmanueldev247 <mailemmydee@gmail.com>
2024-01-21 13:16:38 -0800

    updated: install dependencies

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,21 +1,21 @@
 # Using Puppet to install flask from pip3
-package { 'python3':
+package { 'python3.8':
     ensure => '3.8.10',
 }
 
 package { 'python3-pip':
     ensure  => installed,
-    require => Package['python3'],
+    require => Package['python3.8'],
 }
 
-package { 'Flask':
+package { 'flask':
     ensure   => '2.1.0',
-    provider => 'pip3',
+    provider => 'pip',
     require  => Package['python3-pip'],
 }
 
 package { 'Werkzeug':
-    ensure   => '2.1.1',
-    provider => 'pip3',
+    ensure   => '2.2.2',
+    provider => 'pip',
     require  => Package['python3-pip'],
 }
