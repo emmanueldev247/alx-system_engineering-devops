86799bfe7f7009d9d8bacf663565c38e30dfe741
emmanueldev247 <mailemmydee@gmail.com>
2024-01-21 22:51:46 -0800

    updated: regex error

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,14 +7,14 @@ file { '/etc/ssh/ssh_config':
 file_line { 'identity_file':
   path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
-  match   => '^#?IdentityFile\s+.*$',
-  ensure  => present,
+  match   => '^#?\s*IdentityFile',
+  replace => true
 }
 
 # Ensure PasswordAuthentication line is present and has the correct value
 file_line { 'password_authentication':
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
-  match   => '^#?PasswordAuthentication\s+.*$',
-  ensure  => present,
+  match   => '^#?\s*PasswordAuthentication',
+  replace => true
 }
