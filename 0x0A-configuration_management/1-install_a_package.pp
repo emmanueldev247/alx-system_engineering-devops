2aa6a6760f9faada3936f16bb008defc32fac368
emmanueldev247 <mailemmydee@gmail.com>
2024-01-25 10:25:38 -0800

    updated: dependency

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,18 +3,14 @@ package { 'python3.8':
     ensure => '3.8.10',
 }
 
-package { 'python3-pip':
-    ensure  => installed,
-    require => Package['python3.8'],
-}
-
 package { 'Flask':
-    ensure   => '2.1.0',
-    provider => 'pip',
-    require  => Package['python3-pip'],
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
+# Install Werkzeug as a requirement for Flask
 package { 'Werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
+  require  => Package['Flask'],
 }
