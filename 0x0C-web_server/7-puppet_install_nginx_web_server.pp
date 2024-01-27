1d6ef064e863f00642e3465a196fdaa1706b8198
emmanueldev247 <mailemmydee@gmail.com>
2024-01-27 07:48:05 -0800

    updated: manifest fixed

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,17 @@
 # Install & Configure Nginx web server (w/ Puppet)
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
 package { 'nginx':
     ensure => installed
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
 }
 
 class { 'ufw':
@@ -12,30 +23,56 @@ ufw::allow { 'Nginx HTTP':
     port  => 80,
 }
 
-# Ensure the Nginx service is running and enabled
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+ufw::allow {'SSH':
+    proto => 'tcp',
+    port  => 22,
 }
 
-# Create the index.html file with the specified content
+file { '/var/www':
+  ensure => directory,
+  mode   => '0755',
+  force  => true,
+}
+
 file { '/var/www/html/index.html':
   ensure  => present,
-  content => 'Hello, world!',
-  require => Service['nginx'],
+  content => "Hello World!\n",
 }
 
-# Create the index.html file with the specified content
-file { '/var/www/html/index.html':
+file { '/var/www/html/404.html':
   ensure  => present,
-  content => 'Hello, world!',
-  require => Service['nginx'],
+  content => "Ceci n'est pas une page\n",
 }
 
-# Restart Nginx after updating the configuration
+file { 'Nginx default config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /var/www/html;
+
+        # Add index.php to the list if you are using PHP
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+
+        rewrite ^/redirect_me https://www.youtube.com/ permanent;
+}
+",
+}
+
 exec { 'nginx-restart':
   command     => 'service nginx restart',
-  refreshonly => true,
-  subscribe   => [Service['nginx'], File['/var/www/html/index.html']],
 }
