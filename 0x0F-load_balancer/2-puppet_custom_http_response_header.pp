23f1704dd91f4e61280e6121336d3765befab4d1
emmanueldev247 <mailemmydee@gmail.com>
2024-01-31 08:20:05 -0800

    fixed: add_header section

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,27 @@
 # Add a custom HTTP header with Puppet
-
-exec { 'add nginx stable repo':
-  command => 'sudo add-apt-repository ppa:nginx/stable',
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y update',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# update software packages list
 exec { 'update packages':
   command => 'apt-get update',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-# install nginx
 package { 'nginx':
-  ensure     => 'installed',
+  ensure => installed,
 }
 
-# add custom header
+# Create a basic HTML file
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Configure Nginx to add the custom HTTP header
 file_line { 'add custom header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  line   => "\n\tset $my_host $hostname;\n\tadd_header X-Served-By $my_host;"
+  line   => "\tadd_header X-Served-By ${hostname};",
   after  => 'server_name _;',
 }
 
@@ -28,4 +29,9 @@ file_line { 'add custom header':
 exec { 'restart service':
   command => 'service nginx restart',
   path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+# Ensure Nginx is running
+service { 'nginx':
+  ensure => running,
 }
