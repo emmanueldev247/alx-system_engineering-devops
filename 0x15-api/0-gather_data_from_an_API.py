f0aa656a23ba21ae453468ad919d50b721d1ba11
Emmanuel Ademola <mailemmydee@gmail.com>
2024-03-27 13:12:01 -0700

    fixed: pycodestyle errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,40 +5,45 @@ information about his/her TODO list progress."""
 import requests
 import sys
 
+
 def empApi():
-	base_url = 'https://jsonplaceholder.typicode.com/'
-	emp_id = sys.argv[1]
-	response = requests.get(base_url+f'users/{emp_id}')
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    emp_id = sys.argv[1]
+    response = requests.get(base_url+f'users/{emp_id}')
 
-	if response.status_code == 200:
-		json_data = response.json()
-	else:
-		print('Error:', response.status_code)
-		sys.exit()
-	emp_id = int(emp_id)
-	emp_name = json_data.get("name")
+    if response.status_code == 200:
+        json_data = response.json()
+    else:
+        print('Error:', response.status_code)
+        sys.exit()
 
-	response = requests.get(base_url+f'todos/')
-	if response.status_code == 200:
-		json_data = response.json()
-	else:
-		print('Error:', response.status_code)
-		sys.exit()
+    emp_id = int(emp_id)
+    emp_name = json_data.get("name")
 
-	todo_count = 0
-	completed = []
+    response = requests.get(base_url+f'todos/')
+    if response.status_code == 200:
+        json_data = response.json()
+    else:
+        print('Error:', response.status_code)
+        sys.exit()
 
-	for x in json_data:
-		user_id = x.get("userId")
-		is_completed = x.get("completed")
+    todo_count = 0
+    completed = []
 
-		if user_id == emp_id:
-			todo_count+=1
-			if is_completed:
-				completed.append(x.get("title"))
+    for x in json_data:
+        user_id = x.get("userId")
+        is_completed = x.get("completed")
 
-	print('Employee {} is done with tasks({}/{}):'.format(emp_name, len(completed), todo_count))
-	for i in completed:
-		print("\t {}".format(i))
+        if user_id == emp_id:
+            todo_count += 1
+            if is_completed:
+                completed.append(x.get("title"))
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+                                        emp_name, len(completed), todo_count))
+    for i in completed:
+        print("\t {}".format(i))
+
+
 if __name__ == "__main__":
-	empApi()
+    empApi()
