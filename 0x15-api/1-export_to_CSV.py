5c23c335cf55d82bd535f6f4f896ff87937af143
Emmanuel Ademola <mailemmydee@gmail.com>
2024-03-28 04:46:48 -0700

    updated: blacklist

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Script to export data in the CSV format"""
 
+import csv
 import requests
 import sys
-import csv
 
 
 def empAPI():
@@ -33,24 +33,28 @@ def empAPI():
 
     for x in json_data:
         if x.get("userId") == emp_id:
-            data.append([emp_id, emp_username, x.get("completed"), x.get("title")])
+            data.append([emp_id, emp_username,
+                        x.get("completed"), x.get("title")])
 
     exportCSV(data, emp_id)
 
+
 def exportCSV(data, user_id):
-  """
-  Exports data to a CSV file named after the user ID, with quotes around all data.
+    """
+    Exports data to a CSV file named after the user ID,
+    with quotes around all data.
 
-  Args:
-    data: A list of lists, where each list contains data "USER_ID", "USERNAME",
-    "TASK_COMPLETED_STATUS", and "TASK_TITLE".
-    user_id: The ID of the user whose data are being exported.
+    Args:
+        data: A list of lists, where each list contains data "USER_ID",
+              "USERNAME", "TASK_COMPLETED_STATUS", and "TASK_TITLE".
+        user_id: The ID of the user whose data are being exported.
   """
-  filename = f"{user_id}.csv"
+    filename = f"{user_id}.csv"
 
-  with open(filename, "w", newline="") as csvfile:
-    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-    writer.writerows(data)
+    with open(filename, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        writer.writerows(data)
+
 
 if __name__ == "__main__":
     empAPI()
