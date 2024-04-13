093ade614088dbc7b9b95f79c63c4df2ab91da42
Emmanuel Ademola <mailemmydee@gmail.com>
2024-04-13 10:30:58 -0700

    updated: oauth usage

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,28 @@ def number_of_subscribers(subreddit):
             subreddit - Subreddit to check for
         Return: The number of subs for the subreddit
     """
-    url = f"https://api.reddit.com/r/{subreddit}/about"
 
-    # Setting a custom User-Agent to avoid too many requests error
-    headers = {'User-Agent': 'My user Agent 1.0'}
+    url = f"https://oauth.reddit.com/r/{subreddit}/about"
 
-    # send a GET request to the Reddit API
+    tok = ("eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40c"
+           "WY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2"
+           "VyIiwiZXhwIjoxNzEzMTEzMTk2Ljg3OTg5MSwiaWF0IjoxNzEzMDI2Nzk2Ljg3OTg"
+           "5LCJqdGkiOiJIYWxVLXVwUnZUY2gzT213RGhuc2txUVVsUzdEVFEiLCJjaWQiOiJn"
+           "NHgydmdOQXByS1d1YXNZdjl3NWZ3IiwibGlkIjoidDJfeTQ4dmsybXkyIiwiYWlkI"
+           "joidDJfeTQ4dmsybXkyIiwibGNhIjoxNzEyODgwMTc4MTIyLCJzY3AiOiJlSnlLVn"
+           "RKU2lnVUVBQURfX3dOekFTYyIsImZsbyI6OX0.Xyh5DjwWCJoGJWLQA_2AtAdgCbP"
+           "ykQMjTkxhBjKU_9SpQST0SC5m6AUApDiKk0cC8QDFsdUqqC9nCTjO_TyYLZthUsRE"
+           "dMLPj8jodJEfl6wn9wpUSrjTukVRHh_LZYdDfMY6rh5UlwbX-kPe6LBpGG2LfWhoT"
+           "mghyvodKgcwVo-Tgm7WayP7xNrcLxniRVAqcTaLKpRsL6GkAjKyC1Um6_CA_4rwMi"
+           "o2mMeb6sOWB51Ju2iWgxXAJVHlLN6LPZznAklDQwIEmIgSGHc0D2q7Cz9KTRP8TQi"
+           "oZrb6lKA2CVRAIJQ5KvDLstZLY5wXNNffujdPxpj2joQPKF1kqTRBjQ"
+           )
+
+    headers = {
+        'User-Agent': 'MyAPI/1.0',
+        'Authorization': f'bearer {tok}'
+    }
+
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         sub_count = response.json().get("data").get("subscribers")
