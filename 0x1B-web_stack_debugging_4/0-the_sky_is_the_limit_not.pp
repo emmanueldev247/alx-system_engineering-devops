ccf4676710c77ab2629c25a2718f484a8acc49ea
Emmanuel Ademola <mailemmydee@gmail.com>
2024-04-18 06:02:02 -0700

    updated: puppet-lint compliance

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,12 @@
 
 # Increase ULIMIT value to 4096
 exec {'increase ULIMIT value':
-            command => 'sed -i "s/15/4096/" /etc/default/nginx',
-        path => '/bin/:/usr/bin/:/usr/local/bin'
+    command => 'sed -i "s/15/4096/" /etc/default/nginx',
+    path    => '/bin/:/usr/bin/:/usr/local/bin'
 }
 
 # Restart nginx
 exec {'restart nginx':
-        command => '/usr/sbin/service nginx restart',
-        path => '/usr/sbin/:/sbin:/bin:/usr/bin'
+    command => '/usr/sbin/service nginx restart',
+    path    => '/usr/sbin/:/sbin:/bin:/usr/bin'
 }
